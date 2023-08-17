026f517e45e6ab4344d1394298afa583844eedd4
LuFi-1227 <luizsalvesp@protonmail.com>
2023-08-17 20:32:39 -0300

    Arquivo com os algoritmos de criação e manutenção da árvore rubro negra criados

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main(void){
     printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\nAlgoritmo para plotar arvores rubro negras\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
     Sleep(2000);
     system("cls");
+    
     return 0;
 }
