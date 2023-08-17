9f024cd519a403d69d356880f269fc0ff78c143e
LuFi-1227 <luizsalvesp@protonmail.com>
2023-08-17 20:02:04 -0300

    Arquivo principal criado no repositorio

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <windows.h>
+int main(void){
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\nAlgoritmo para plotar arvores rubro negras\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+    Sleep(2000);
+    system("cls");
+    return 0;
+}
